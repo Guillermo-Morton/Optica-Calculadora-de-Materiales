f01f7094826722fe23df26fe8c34530ed0910967
Guillermo Morton <guillermort2000@gmail.com>
2021-08-12 02:42:35 -0300

    Se agregaron materiales con rangos extendidos y la logica para filtrarlos- material amplitude

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DataTable, { createTheme } from 'react-data-table-component'
+import DataTable from 'react-data-table-component'
 
 function App() {
   const [esferico, setEsferico]= useState('')
@@ -9,6 +9,7 @@ function App() {
   const [esfericoValor, setEsfericoValor]= useState(true)
   const [cilindricoValor, setCilindricoValor]= useState(true)
   const [resultado, setResultado]= useState(0)
+  const [dioptrias, setDioptrias]= useState(0)
   const [filtrados, setFiltrados]= useState([])
   const columnas= [
     {
@@ -121,14 +122,72 @@ function App() {
       cilindrico: 2 ,
       maxDioptrias:6
     },
+    // Rango extendido
+    {
+      nombre: 'AMPLITUDE HD BLUE RE',
+      precio: '$6400 / $6800 | si supera cil -3 $7500',
+      esfericoPositivo: {
+        esfericoMin: 4.25,
+        esfericoMax: 6,
+        cilindrico: -2
+      },
+      esfericoNegativo: {
+        esfericoMin: -10.00,
+        esfericoMax: -6.25,
+        cilindrico: -2
+      },
+      esfericoNeutro:{
+        esfericoMin: -6,
+        esfericoMax: 0,
+        cilindricoMin: -4,
+        cilindricoMax: -2.25
+      }
+    },
+    {
+      nombre: 'AMPLITUDE HD DUAL BLUE RE',
+      precio: '$9500',
+      esfericoPositivo: {
+        esfericoMin: 4.25,
+        esfericoMax: 6,
+        cilindrico: -2
+      },
+      esfericoNegativo: {
+        esfericoMin: -10.00,
+        esfericoMax: -6.25,
+        cilindrico: -2
+      },
+      esfericoNeutro:{
+        esfericoMin: -6,
+        esfericoMax: 0,
+        cilindricoMin: -3,
+        cilindricoMax: -2.25
+      }
+    },
   ]
   const handleFilter = (esferico, cilindrico) =>{
-    setFiltrados(materiales.filter(material=> (esferico>=material.esferico && esferico <= 4 ? true : false) && (cilindrico <= material.cilindrico && cilindrico >= -2 ? true : false))) 
+    setFiltrados(materiales.filter(material=>
+       ((
+        //  Mediciones para materiales de stock
+        esferico>=material.esferico && esferico <= 4 ? true : false)
+        && (cilindrico <= material.cilindrico && cilindrico >= -2 ? true : false)
+        && material.maxDioptrias && dioptrias <= material.maxDioptrias)
+        // Mediciones para materiales con rango extendido
+
+        || ((material.esfericoPositivo && esferico >= material.esfericoPositivo.esfericoMin && esferico <= material.esfericoPositivo.esfericoMax)
+        && cilindrico >= material.esfericoPositivo.cilindrico )
+
+        || ((material.esfericoNegativo && esferico >= material.esfericoNegativo.esfericoMin && esferico <= material.esfericoNegativo.esfericoMax)
+        && cilindrico >= material.esfericoNegativo.cilindrico)
+
+        || ((material.esfericoNeutro && esferico >= material.esfericoNeutro.esfericoMin && esferico <= material.esfericoNeutro.esfericoMax)
+        && cilindrico >= material.esfericoNeutro.cilindricoMin && cilindrico <= material.esfericoNeutro.cilindricoMax )
+        )) 
   }
   const handleSubmit = (e,esferico, cilindrico)=>{
     e.preventDefault()
     setResultado(`ESF ${esferico + cilindrico} con CIL ${-cilindrico> 0 ? '+'+-cilindrico: -cilindrico }`)
     handleFilter(esferico + cilindrico, -cilindrico> 0 ? +-cilindrico : -cilindrico )
+    setDioptrias((esferico < 0 ? -esferico : esferico ) + (cilindrico < 0 ? -cilindrico : cilindrico))
   }
   const handleChange = (value, state, valor) => {
     state(!valor ? (value<0 ? value : -value ): value)
@@ -141,7 +200,7 @@ function App() {
   },[cilindricoValor])
   return (
     <div className='container my-4 d-flex justify-content-center align-items-center flex-column'>
-      <form onSubmit={(e)=>handleSubmit(e,esferico, cilindrico)}>
+      <form onSubmit={(e)=>handleSubmit(e,esferico || 0, cilindrico || 0)}>
         <div className='my-2'>
           <div>
             <button onClick={()=> setEsfericoValor(true)} className={`btn btn-success py-1 ${esfericoValor ? 'disabled':''}`}>Positivo</button>
@@ -149,7 +208,7 @@ function App() {
           </div>
           <div className='d-flex flex-column'>
           <label>Esferico</label>
-          <input className='' type="number" placeholder='Ingresa una graduacion' onChange={e=> handleChange(parseFloat(e.target.value),setEsferico, esfericoValor)} value={esferico} name="" id="" />
+          <input className='' type="number" placeholder='Ingresa una graduacion' onChange={e=> handleChange(parseFloat(e.target.value) || '',setEsferico, esfericoValor)} value={esferico} name="" id="" />
           </div>
         </div>
         <span>+</span>
@@ -160,7 +219,7 @@ function App() {
           </div>
           <div className='d-flex flex-column'>
           <label>Cilindrico</label>
-          <input className='' type="number" placeholder='Ingresa una graduacion' onChange={e=> handleChange(parseFloat(e.target.value),setCilindrico, cilindricoValor)} value={cilindrico} name="" id="" />
+          <input className='' type="number" placeholder='Ingresa una graduacion' onChange={e=> handleChange(parseFloat(e.target.value) || '',setCilindrico, cilindricoValor)} value={cilindrico} name="" id="" />
           </div>
         </div>
       <button className='btn btn-primary mt-3'>Operar</button>
